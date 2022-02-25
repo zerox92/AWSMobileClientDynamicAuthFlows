98cb68b28e33e0b16d0344d2746b7ae2910a4bf7
Zeeshan Anjum <zeerox92@gmail.com>
2022-02-25 15:28:38 +0000

    Initial Commit

---
+++ iOS-Sample-Different-Auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Sample-Different-Auth
+//
+//  Created by Anjum, Zeeshan on 25/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
