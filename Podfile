7599002760654e8070780cb8328da08aea3009a3
Zeeshan Anjum <zeerox92@gmail.com>
2022-02-25 16:04:24 +0000

    - Initial config made

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iOS-Sample-Different-Auth' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  
+  inhibit_all_warnings!
+
+  pod 'AWSMobileClient'
+  pod 'AWSAuthUI'
+  pod 'AWSUserPoolsSignIn'
+
+end
--- iOS-Sample-Different-Auth/ViewController.swift
+++ iOS-Sample-Different-Auth/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import AWSMobileClient
+import AWSAuthCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+ 
+        do {
+            try AWSConfiguration.configure()
+            print("Yayy SDK configured")
+        } catch _ {
+           print("Some kind of exception")
+        }
     }
-
 
 }
 
+class AWSConfiguration {
+    enum AppError: Error {
+        case configurationDontExist
+    }
+    
+    static func retrieve(forResource: String) throws -> Data {
+        guard let path = Bundle(for: self).path(forResource: forResource, ofType: "json") else {
+            throw AppError.configurationDontExist
+        }
+
+        let url = URL(fileURLWithPath: path)
+        return try Data(contentsOf: url)
+    }
+    
+    static func configure() throws {
+        let configurationData = try retrieve(forResource: "awsconfiguration")
+        let authConfig = (try? JSONSerialization.jsonObject(with: configurationData, options: [])
+            as? [String: Any]) ?? [:]
+        AWSInfo.configureDefaultAWSInfo(authConfig)
+        AWSMobileClient.default().initialize { _, error in
+            guard error == nil else {
+                print("Error initializing AWSMobileClient. Error: \(error!.localizedDescription)")
+                return
+            }
+            print("AWSMobileClient initialized.")
+        }
+    }
+}
